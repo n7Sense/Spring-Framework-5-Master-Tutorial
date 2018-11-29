612a9c4e685d955a9d6ffd12a20ca3d52494a365
n7Sense <rajbharrahul1994@gmail.com>
2018-11-29 11:03:01 +0530

    correction in histry of SF

--- A. Histry/histry.java
+++ A. Histry/histry.java
@@ -12,13 +12,13 @@ We can reduce investimate cost.
 
 Q. Why they named to Spring .?
 	
-	They are comparing Spring with EJB, EJB is like WINTER Sesion, and after winter the next coming sesion is SPRING.
+	They are comparing Spring with EJB, EJB is like WINTER Season, and after winter the next coming season is SPRING.
 	WINTER is not fine for US People because in Winter there used to get -20 Degree in America,
-	so EJB they are comparing with Winter sesion and the next sesion is SPRING.
-	In spring sesion they used to get 5-Degree or 0 degree thats fine for US people.
+	so EJB they are comparing with Winter "season and the next "season is SPRING.
+	In spring season they used to get 5-Degree or 0 degree thats fine for US people.
 
 	No Resion behind it the are saying it Spring is EASY by comparing with EJB.
 
-Spring is a Sesion, thats why we can see the Spring Logo i.e LEAF so just they are comparing with Sesion. no Resion behind it.
+Spring is a Season, thats why we can see the Spring Logo i.e LEAF so just they are comparing with Season. no Resion behind it.
 
 
