121ed5fa1496749713e3a6b7c5bb37c40c26dedd
n7Sense <rajbharrahul1994@gmail.com>
2018-12-04 14:47:21 +0530

    What container do.?

---
+++ D. Container/What Container do.java
@@ -0,0 +1,8 @@
+	What Container do.?
+=============================
+
+1. Container will create instances for POJO class with null state.
+
+2. Manage the Life-Cycle of POJO classes.
+
+3. Into POJO Classes, we can do Dependencies injection.
