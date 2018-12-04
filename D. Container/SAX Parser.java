2c7587186856d910e58e9773f428ba587f5b39d8
n7Sense <rajbharrahul1994@gmail.com>
2018-12-04 15:34:01 +0530

    What SAX parser do.?

---
+++ D. Container/SAX Parser.java
@@ -0,0 +1,13 @@
+	SAX Parser
+================
+	
+	1. XSD or DTD validate the Spring XMl.
+
+	2. SAX Parser is present inside the IoC container.
+
+	3. IoC container read the Spring XMl file with the help of SAX Parser.
+
+	4. the SAX Parser check the well formed and validation of document.
+
+	5. if the parser foud any mistake, not well formed xml documents, then it will throws an Exception 
+		
