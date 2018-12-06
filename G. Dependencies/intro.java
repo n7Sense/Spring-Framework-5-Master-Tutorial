f82738d0583e48770b16178fb5aa2c6759d96241
n7Sense <rajbharrahul1994@gmail.com>
2018-12-06 16:22:02 +0530

    how to apply dependencies to all layer

---
+++ G. Dependencies/intro.java
@@ -0,0 +1,5 @@
+How to Apply Dependencies To POJO
+===================================
+
+In Spring MVC , using Spring MVC XML Configuration file we can apply all dependencies to POJO classes through IoC container
+.
