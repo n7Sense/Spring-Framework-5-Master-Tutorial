1c4c1d6b58541b38ea1859b56df5db83b887ea3f
n7Sense <rajbharrahul1994@gmail.com>
2018-12-08 20:18:35 +0530

    Data Type For Attribute

--- I. What Is XML/4. How to declare Attribute list.java
+++ I. What Is XML/4. How to declare Attribute list.java
@@ -55,7 +55,31 @@ Attribute Specifiier
 				<!ATTLIST employee phone CDATA #IMPLIED "979271935" > this is default value for default specifier
 				<!ATTLIST employee empId CDATA #FIXED  "1520000E01">
 
-		
+Data Type For Attribute
+========================
+
+	1. CDATA : 	CDATA is character data (text and not markup). It is a String Attribute Type.
+
+	2. 	ID : It is a unique identifier of the attribute. It should not appear more than once. It is a Tokenized Attribute Type.
+
+	3. 	IDREF : It is used to reference an ID of another element. It is used to establish connections between elements.
+				It is a Tokenized Attribute Type.
+
+	4. 	IDREFS : It is used to reference multiple ID. It is a Tokenized Attribute Type.
+
+	5. 	ENTITY : It represents an external entity in the document. It is a Tokenized Attribute Type.
+
+	6. 	ENTITIES : It represents a list of external entities in the document. It is a Tokenized Attribute Type.
+
+	7. 	NMTOKEN : It is similar to CDATA and the attribute value consists of a valid XML name. It is a Tokenized Attribute Type.
+
+	8. 	NMTOKENS : It is similar to CDATA and the attribute value consists a list of valid XML name.
+					It is a Tokenized Attribute Type.
+
+	9. NOTATION : An element will be referenced to a notation declared in the DTD document. It is an Enumerated Attribute Type.
+
+	10.	Enumeration : It allows defining a specific list of values where one of the values must match.
+						It is an Enumerated Attribute Type.
 
 
 
