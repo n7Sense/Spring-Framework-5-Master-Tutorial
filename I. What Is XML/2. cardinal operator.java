00f0604d8a705c03372475cedd8cb377b4c6d6e0
n7Sense <rajbharrahul1994@gmail.com>
2018-12-08 15:33:21 +0530

    Cardinal operator in DTD

---
+++ I. What Is XML/2. cardinal operator.java
@@ -0,0 +1,18 @@
+Cardinal Operator
+===================
+
+* 	  	Indicate 		0 to n
+
++ 		Indicate 		1 to n
+
+? 		Indicate 		0 OR 1
+
+
+	Example:
+
+		<!ELEMENT books (book*) > //chance of 0 to many books are there
+ 		<!ELEMENT book ((book-name | title-name ), price, author+, publication? ) >
+
+ 		author: for one book  1 to many author are available.
+ 		publication : not mandatory it will or will not.
+ 		(book-name | title-name ): any one of them
