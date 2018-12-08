ac0f07692d2a2d0becb455a472932e258ec52b09
n7Sense <rajbharrahul1994@gmail.com>
2018-12-08 15:10:03 +0530

    External and Internal DTD

--- I. What Is XML/dtd.java
+++ I. What Is XML/dtd.java
@@ -29,6 +29,46 @@ How to map or how to link DTD in XMl documents.?
 	there are have 2-types of DTD
 		1. Internal DTD
 			if we writing the DTD within the xml documents.
+			+-------------------------------------------------------------------------+	
+				<?xml version="1.0" encoding="utf-8"?>
+				<!DOCTYPE employee [
+				    <!NOTATION vrml PUBLIC "VRML 1.0">
+				    <!ELEMENT employee (id, name, salary, phone, yog) >
+				    <!ELEMENT phone(#PCDATA) >
+				    <!ELEMENT salary(#PCDATA) >
+				]>
+				<employee>
+					<id>100</id>
+					<name>Rahul</name>
+					<salary>5000</salary>
+					<phone>97698 45623</phone>
+					<yog>MadhuSudan</yog>
+				</employee>
+			+--------------------------------------------------------------------------+
 
 		2. External DTD
 			if we are writing the DTD in separate text bases documents, and linking that to XML documents.
+			External DTD are 2-types
+				1. Private DTD
+					If any DTD is specific to perticular XML documents, that type of DTD is called Private DTD.
+					+--------------------------------------------------------------------------------------+
+					|		<!DOCTYPE rootEmement PUBLIC "-//vendorName//version//EN" "DTDFileName.DTD">   |
+					+--------------------------------------------------------------------------------------+
+					
+					"-//vendorname//version//EN"
+						plus(+) symbol indicating Registered wth ISO
+						minus(-) symbol indicating Not Registered with ISO
+						EN indicating this DTD is written in English language
+						NOTE: if we not provide the vendorname, version and language then must specify " " double quote.
+						NOTE: this information is not required in Private DTD
+
+					example: How to link in external dtd inside xml file
+							<!DOCTYPE rootEmement PUBLIC "-//nSense/1.0//EN" "C:\user\nSense\dtd\DtdFileName.dtd">
+
+				2. Public DTD
+					If any DTD is not specific to perticular XML documents, that type of DTD is called Public DTD.
+					+------------------------------------------------------------------------------+
+					|		<!DOCTYPE rootEmement SYSTEM "C:\user\nSense\dtd\DtdFileName.dtd">	   |
+					+------------------------------------------------------------------------------+
+				
+
