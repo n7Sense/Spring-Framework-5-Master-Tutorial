66fb51ab9277bf2e1fbfea3869effed9a6238893
n7Sense <rajbharrahul1994@gmail.com>
2018-12-09 14:27:09 +0530

    XSD datatypes hirarchey

---
+++ I. What Is XML/6. XSD Data types.java
@@ -0,0 +1,26 @@
+XSD Data types
+==============
+
+												Data Types
+													+
+													|
+					+-------------------------------+-------------------------------+
+					|																|
+				Simple Types 													Complex Types
+					+																+
+					|																|
+		+------------+------------------+						+-------------------+-------------------+
+		|						 		|						|					|					|
+User Derived Types					Built In Types(44)		   EMPTY			Simple Content 			Complex Content
+		+								+																|
+		|								|																|> Sequence
+		|> Atomic						|> Primitive													|
+		|								|																|> Choice
+		|> Non Atomic					|> Derived														|
+																										|> All
+
+
+
+
+
+	
