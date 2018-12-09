28fe91afd0276f863559ddbaa9261511e665b65a
n7Sense <rajbharrahul1994@gmail.com>
2018-12-09 14:42:24 +0530

    Derived XSD data types

--- I. What Is XML/6. XSD Data types.java
+++ I. What Is XML/6. XSD Data types.java
@@ -27,3 +27,9 @@ Primitive Data Types
 	string, boolean, decimal, float, double, duration, dateTime, time, date, gYearMonth, gYear, gDay, gMonth,
 	hexBinary, base64Binary, anyURI, QName, Notation
 
+
+Derived Data Types
+===================
+	normalizedString, token, language, IDREFS, ENTITIES, NMTOKEN, NMTOKENS, Name NCName, ID, IDREF, ENTITY,
+	integer, nonPositiveInteger, negativeInteger, long, int, short, byte, nonNegativeInteger, unsignedLong,
+	unsignedInt, unsignedShort, unsignedByte, positiveinteger
