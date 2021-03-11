a0800e61a59f4895e2cba5a9f2f8327c0ac804c3
Rahul H. Rajbhar <rajbharrahul1994@gmail.com>
2021-03-11 11:19:08 +0530

    Update 5. Using newInstance() method of Constructor.java

--- F. Create Object In Java/5. Using newInstance() method of Constructor.java
+++ F. Create Object In Java/5. Using newInstance() method of Constructor.java
@@ -1,7 +1,9 @@
  Using newInstance() method of Constructor
  ==========================================
 
-5) Using newInstance() method of Constructor class : This is similar to the newInstance() method of a class. There is one newInstance() method in the java.lang.reflect.Constructor class which we can use to create objects. It can also call parameterized constructor, and private constructor by using this newInstance() method.
+5) Using newInstance() method of Constructor class : This is similar to the newInstance() method of a class.
+  There is one newInstance() method in the java.lang.reflect.Constructor class which we can use to create objects.
+  It can also call parameterized constructor, and private constructor by using this newInstance() method.
 
 Both newInstance() methods are known as reflective ways to create objects. In fact newInstance() method of Class internally uses newInstance() method of Constructor class.
 
